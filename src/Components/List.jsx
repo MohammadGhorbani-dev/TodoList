20ebc892116cd581e64ae9df865f5bd13bd50ac7
MohammadGhorbani-dev <mohamad.ghorbani.official@gmail.com>
2023-12-23 00:04:31 +0330

    update

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,17 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TextContext } from "../App";
 
 export default function List() {
   const { list, setList } = useContext(TextContext);
 
+  const [editIndex, setEditIndex] = useState(null);
+  const [editedText, setEditedText] = useState("");
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setEditedText(list[index]);
+  };
+
+  const handleEditConfirmation = () => {
+    const newList = [...list];
+    newList[editIndex] = editedText;
+    setList(newList);
+    setEditIndex(null);
+  };
+
+  const handleEditCancel = () => {
+    setEditIndex(null);
+  };
+
   const handleDelete = (index) => {
     const newList = [...list];
     newList.splice(index, 1);
-    setList(newList);
-  };
-  const handleEdit = (index) => {
-    const newList = [...list];
-    newList[index] = prompt("Edit Text: ", newList[index]);
     setList(newList);
   };
 
@@ -26,26 +40,61 @@ export default function List() {
         <div className="flex justify-center py-10 text-2xl">empty list</div>
       ) : (
         list.map((item, index) => {
+          const isEditing = index === editIndex;
           return (
             <div key={index} className="flex m-auto justify-between">
-              <div
-                className={`text-white items-center h-fit py-2.5 rounded-2xl my-4 px-1 min-w-[19rem] max-w-[19rem] break-words shadow-xl`}
-                dir={getDirection(item)}
-              >
-                {item}
-              </div>
-              <button
-                onClick={() => handleDelete(index)}
-                className="m-auto w-16 p-1 mx-4 h-12 bg-gray-600 rounded-xl hover:bg-red-500 text-white"
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => handleEdit(index)}
-                className="w-16 h-12 m-auto bg-gray-600 rounded-xl text-white hover:bg-gray-400"
-              >
-                Edit
-              </button>
+              {isEditing ? (
+                <div
+                  className={`text-white items-center h-fit py-2.5 px-4 bg-opacity-20 bg-violet-950 rounded-2xl my-4 min-w-[19rem] max-w-[19rem] break-words shadow-xl`}
+                >
+                  <input
+                    type="text"
+                    value={editedText}
+                    onChange={(e) => setEditedText(e.target.value)}
+                    className={`w-full outline-none  h-fit bg-transparent text-white ${
+                      getDirection(item) === "rtl" ? "text-right" : ""
+                    }`}
+                  />
+                </div>
+              ) : (
+                <div
+                  className={`text-white items-center h-fit py-2.5  bg-opacity-20 bg-violet-950 rounded-2xl my-4 px-4 min-w-[19rem] max-w-[19rem] break-words shadow-xl`}
+                  dir={getDirection(item)}
+                >
+                  {item}
+                </div>
+              )}
+              {isEditing ? (
+                <>
+                  <button
+                    onClick={handleEditConfirmation}
+                    className="w-16 h-12 m-auto p-1 mx-4 bg-gray-600 rounded-xl text-white hover:bg-green-500"
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={handleEditCancel}
+                    className="w-16 h-12 m-auto bg-gray-600 rounded-xl text-white hover:bg-gray-400"
+                  >
+                    Cancel
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    onClick={() => handleDelete(index)}
+                    className="m-auto w-16 p-1 mx-4 h-12 bg-gray-600 rounded-xl hover:bg-red-500 text-white"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => handleEdit(index)}
+                    className="w-16 h-12 m-auto bg-gray-600 rounded-xl text-white hover:bg-gray-400"
+                  >
+                    Edit
+                  </button>
+                </>
+              )}
             </div>
           );
         })
